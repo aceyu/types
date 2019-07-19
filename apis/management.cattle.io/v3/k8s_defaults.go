a0869d78db3fdfbd632f1b4b847fe03fcaddc016
aceyu <cydiay@gmail.com>
2019-07-19 13:49:43 +0800

    modify plugins docker version to 17.12-1

--- apis/management.cattle.io/v3/k8s_defaults.go
+++ apis/management.cattle.io/v3/k8s_defaults.go
@@ -126,7 +126,7 @@ var (
 			Jenkins:       m("rancher/pipeline-jenkins-server:v0.1.0"),
 			JenkinsJnlp:   m("jenkins/jnlp-slave:3.10-1-alpine"),
 			AlpineGit:     m("rancher/pipeline-tools:v0.1.9"),
-			PluginsDocker: m("plugins/docker:17.12"),
+			PluginsDocker: m("plugins/docker:17.12-1"),
 			Minio:         m("minio/minio:RELEASE.2018-05-25T19-49-13Z"),
 			Registry:      m("registry:2"),
 			RegistryProxy: m("rancher/pipeline-tools:v0.1.9"),
